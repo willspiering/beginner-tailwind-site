6aa82bb04541057491b5c64da7cb740627e11d9a
Will Spiering <wspiering@skylinetechnologies.com>
2020-10-19 18:21:25 -0700

    setup tailwind for react

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="py-20 px-40 bg-blue-800 min-h-screen">
+			<img src={logo} alt="React logo" />
+			<h1 className="text-center text-6xl font-bold text-blue-500">React with Tailwind</h1>
+		</div>
+	);
 }
 
 export default App;
